2378d45ee559ceb493317c21d1564126df6cdf84
adwinmbd <adwin.mbidi@outlook.com>
2020-12-29 00:16:33 +0100

    feat: add prettier to react-class-todos

--- react-class-todos/src/server.js
+++ react-class-todos/src/server.js
@@ -5,40 +5,40 @@ export function makeServer({ environment = "development" } = {}) {
     environment,
 
     models: {
-        todo: Model,
+      todo: Model,
     },
 
     seeds(server) {
-        server.create("todo", { text: "Buy groceries" });
-        server.create("todo", { text: "Walk the dog" });
-        server.create("todo", { text: "Do laundry" });
+      server.create("todo", { text: "Buy groceries" });
+      server.create("todo", { text: "Walk the dog" });
+      server.create("todo", { text: "Do laundry" });
     },
 
     routes() {
       this.namespace = "api";
       this.timing = 2000;
 
-        this.get("/todos", (schema) => {
-            return schema.todos.all();
-        });
+      this.get("/todos", (schema) => {
+        return schema.todos.all();
+      });
 
-        this.post("/todos", (schema, request) => {
-            // let todo = JSON.parse(request.requestBody).data;
-            let todo = JSON.parse(request.requestBody);
-            return schema.todos.create(todo);
-        });
+      this.post("/todos", (schema, request) => {
+        // let todo = JSON.parse(request.requestBody).data;
+        let todo = JSON.parse(request.requestBody);
+        return schema.todos.create(todo);
+      });
 
-        this.patch("/todos/:id", (schema, request) => {
-            // let data = JSON.parse(request.requestBody).data;
-            let data = JSON.parse(request.requestBody);
-            let id = request.params.id;
-            return schema.todos.find(id).update(data);
-        });
+      this.patch("/todos/:id", (schema, request) => {
+        // let data = JSON.parse(request.requestBody).data;
+        let data = JSON.parse(request.requestBody);
+        let id = request.params.id;
+        return schema.todos.find(id).update(data);
+      });
 
-        this.delete("/todos/:id", (schema, request) => {
-            let id = request.params.id;
-            return schema.todos.find(id).destroy();
-        });
+      this.delete("/todos/:id", (schema, request) => {
+        let id = request.params.id;
+        return schema.todos.find(id).destroy();
+      });
     },
   });
 
