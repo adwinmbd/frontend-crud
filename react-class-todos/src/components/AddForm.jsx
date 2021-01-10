44aa590b4b668254e08f332f234954f96090566f
adwinmbd <adwin.mbidi@outlook.com>
2021-01-10 20:05:47 +0100

    refractor: upgrade vitejs in react-class-todos

--- react-class-todos/src/components/AddForm.jsx
+++ react-class-todos/src/components/AddForm.jsx
@@ -2,17 +2,32 @@ import React, { Component } from "react"
 
 export default class AddForm extends Component {
 
+
+    constructor(props) {
+        super(props);
+        this.onChange = this.onChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    
+        this.state = {
+            text:''
+        }
+    }
+
+    /*
     state = {
         text: ""
     };
+    */
 
-    onChange = e => {
+    // onChange = e => {
+    onChange(e){
         this.setState({
             [e.target.name]: e.target.value
         });
     };
 
-    handleSubmit = e => {
+    // handleSubmit = e => {
+    handleSubmit(e){
         e.preventDefault();
         let newTodo ={text:this.state.text}
         this.props.addTodoProps(newTodo);
@@ -26,25 +41,18 @@ export default class AddForm extends Component {
         const {text}=this.state;
         return (
             <form onSubmit={this.handleSubmit}>
-                <div className="field has-addons has-addons-centered">
-                    <div className="control">
-                        <input
-                            className="input"
-                            placeholder="Add a todo..." 
-                            name="text"
-                            value={text} 
-                            onChange={this.onChange} 
-                            type="text"
-                        />
-                    </div>
-                    <div className="control">
-                        <button className="button is-primary">
-                            <span className="icon is-small">
-                                <img className="mr-1" height="32" width="32" src="./src/assets/add-circle-outline.svg" />
-                            </span>
-                        </button>
-                    </div>
-                </div>
+              <p className="grouped">
+                <input 
+                    type="text" 
+                    placeholder="Add a todo ..." 
+                    name="text" 
+                    value={text} 
+                    onChange={this.onChange} 
+                />
+                <button className="button primary icon-only">
+                  <img height="18" width="18" src="./src/assets/add-circle-outline.svg" />
+                </button>
+              </p>
             </form>
         )
     }
